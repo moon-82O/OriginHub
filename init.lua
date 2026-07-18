51662842bc150a7b0c5513be20e8cabc34bc412d
Moon <realmoon820@gmail.com>
2026-07-18 02:06:41 +0200

    Update init.lua

--- init.lua
+++ init.lua
@@ -1,15 +1,9 @@
---[[
-    Origin Hub - Main Script
-    Version: 1.0.0
-    Place your main hub script here
-]]
-
 local Origin = {}
 Origin.Version = "1.0.0"
 Origin.Name = "ORG"
 
 print("[ORIGIN] Hub initialized v" .. Origin.Version)
 
--- Your script here
+-- soon
 
 return Origin
